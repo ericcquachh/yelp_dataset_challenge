df9969bcf4da5e90441d8ef7050f6219d3b2eab3
Colin Garcia <colin.garcia@colin-garci-ltm.internal.salesforce.com>
2015-11-11 17:03:47 -0800

    jk, I dun goofed

--- retrieval.py
+++ retrieval.py
@@ -4,6 +4,7 @@ import os
 import nltk
 
 DATA_PATH = "/Users/colin.garcia/Desktop/yelp_dataset_challenge_academic_dataset/"
+#DATA_PATH = "/Users/colin.garcia/Desktop/yelp_dataset_challenge/"
 file_name = "yelp_academic_dataset_review.json"
 
 """ Retrieve the number of lines in a json file. 
@@ -17,7 +18,7 @@ def num_lines():
 	return counter
 
 
-""" Retrieve the stars of the array. """
+""" Retrieve the stars of each review in their corresponding array. """
 def star_array():
 	array_of_stars = [0 for i in range(1569264)] # just gonna hardcode this lol
 	with open(file_name) as json_file:
@@ -27,6 +28,17 @@ def star_array():
 			array_of_stars[i] = stars
 	return array_of_stars
 
+""" Put the text as elements of an array. """
+def text_array(array):
+	array_of_text = [0 for i in range(1569264)]
+	with open(file_name) as json_file:
+		for i, line in enumerate(json_file):
+			parsed_json = json.loads(line)
+			text = parsed_json['text']
+			array_of_text[i] = text
+	return array_of_text
 
 
-
+def main():
+	stars_array = star_array()
+	review_text = text_array(stars_array)
