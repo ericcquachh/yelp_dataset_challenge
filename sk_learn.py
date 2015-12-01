7fa39b957813e1cf23adc34a22c811ced9204241
Colin Garcia <colinrgarcia@gmail.com>
2015-12-01 13:18:09 -0800

    adding a file, currently does nothing additional

---
+++ sk_learn.py
@@ -0,0 +1,15 @@
+""" USING SKLEARN CLASSIFIER"""
+
+from sklearn.svm import LinearSVC
+from nltk.classify.scikitlearn import SklearnClassifier
+
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+pipeline = Pipeline([('tfidf', TfidfTransformer()), 
+					 ('chi2', SelectKBest(chi2, k=1000)),
+					 ('nb', MultinomialNB())])
+
+classif = SklearnClassifier(pipeline)
